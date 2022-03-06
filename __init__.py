fdb6af10b82b4c851ed5dc12cd345e4745b42e5a
bcantera <45189283+bcantera@users.noreply.github.com>
2022-03-06 19:31:10 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
